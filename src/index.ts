7d619496e193080845a207f932b7c1f41069ea42
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2026-02-23 15:23:21 +0900

    fix: fixed api deploy error

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export { throwIfError } from './throw-if-error';
-export type { BetterAuthError } from './throw-if-error';
-export { passwordPluginInterface } from './plugins/password';
-export { oauthApplicationPluginInterface } from './plugins/oauth-application';
+/* eslint-disable import/no-unresolved */
+export { throwIfError } from './throw-if-error.js';
+export type { BetterAuthError } from './throw-if-error.js';
+export { passwordPluginInterface } from './plugins/password.js';
+export { oauthApplicationPluginInterface } from './plugins/oauth-application.js';
