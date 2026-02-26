c2251fe9201fb3553775d08a84778054e4212fcf
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2026-02-26 22:10:09 +0900

    remove: removed unused eslint comment

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-unresolved */
 export { throwIfError } from './throw-if-error.js';
 export type { BetterAuthError } from './throw-if-error.js';
 export { passwordPluginInterface } from './plugins/password.js';
